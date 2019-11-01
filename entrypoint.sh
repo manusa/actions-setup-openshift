76e0623547908a1b61ed9ed4e3decf86e913a6b5
Marc Nuri <marc@marcnuri.com>
2019-11-01 08:53:18 +0100

    #2: Renamed input minishift variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 set -o pipefail
 
 download_minishift() {
-  echo Downloading Minishift $INPUT_VERSION
-  VERSION=${INPUT_VERSION//[a-zA-Z]}
-  curl -Lo minishift.tgz "https://github.com/minishift/minishift/releases/download/$INPUT_VERSION/minishift-$VERSION-linux-amd64.tgz"
+  echo Downloading Minishift $INPUT_MINISHIFT_VERSION
+  VERSION=${INPUT_MINISHIFT_VERSION//[a-zA-Z]}
+  curl -Lo minishift.tgz "https://github.com/minishift/minishift/releases/download/$INPUT_MINISHIFT_VERSION/minishift-$VERSION-linux-amd64.tgz"
   tar -xf minishift.tgz
   mv ./minishift-*/minishift minishift
   chmod +x minishift
