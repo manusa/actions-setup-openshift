62d4fc9a2ad25df32f81d381e26b3c542209a8d1
Marc Nuri <marc@marcnuri.com>
2019-11-02 05:34:27 +0100

    #4: Global variable for MINISHIFT_HOME

--- src/install.js
+++ src/install.js
@@ -7,9 +7,10 @@ const core = require('@actions/core');
 
 const install = async minishiftTar => {
   const extractedMinishift = await tc.extractTar(minishiftTar);
-  const minishiftDirectory = fs.readdirSync(extractedMinishift)[0];
-  child_process.execSync(`chmod +x ${extractedMinishift}/${minishiftDirectory}/minishift`);
-  core.addPath(`${extractedMinishift}/${minishiftDirectory}`);
+  const minishiftDirectory = `${extractedMinishift}${fs.readdirSync(extractedMinishift)[0]}`;
+  child_process.execSync(`chmod +x ${minishiftDirectory}/minishift`);
+  core.exportVariable("MINISHIFT_HOME", minishiftDirectory);
+  core.addPath(minishiftDirectory);
   const minishiftVersion = child_process.execSync(`minishift version`)
     .toString()
     .replace(/[\n\r]/g, '');
